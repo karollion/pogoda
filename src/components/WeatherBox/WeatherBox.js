2cb73c2f6ce690ed0410c3d3de73220166da46ae
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-08-27 19:05:40 +0200

    Init project

---
+++ src/components/WeatherBox/WeatherBox.js
@@ -0,0 +1,22 @@
+import PickCity from '../PickCity/PickCity';
+import WeatherSummary from '../WeatherSummary/WeatherSummary';
+import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
+
+const WeatherBox = props => {
+
+  const handleCityChange = useCallback(city =>{
+    console.log(city);
+  }, []);
+ 
+
+  return (
+    <section>
+      <PickCity action={handleCityChange} />
+      <WeatherSummary />
+      <Loader />
+    </section>
+  )
+};
+
+export default WeatherBox;
